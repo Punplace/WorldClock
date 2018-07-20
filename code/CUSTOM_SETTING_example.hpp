3e342b8921ade5f68d7ca88217f603b294f38a07
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-07-20 15:01:05 +0800

    pass LED test

--- code/CUSTOM_SETTING_example.hpp
+++ code/CUSTOM_SETTING_example.hpp
@@ -7,9 +7,13 @@
  */
 # pragma once
 
+// GPIO setting
+const uint8_t LED_WIRED_PIN = D5;
+
+
 // WiFi setting
 const char * MY_WIFI_SSID = "WildfootW";
-const char * MY_WIFI_PASSWORD = "12345678";
+const char * MY_WIFI_PASSWORD = "";
 
 // NTP server
 const char * NTP_SERVER_DOMAIN_NAME = "1.tw.pool.ntp.org";
--- code/main/main.ino
+++ code/main/main.ino
@@ -8,6 +8,8 @@
 
 #include <ESP8266WiFi.h>
 #include "CUSTOM_SETTING.hpp"
+#include <Adafruit_NeoPixel.h>
+//#include "library/Adafruit_NeoPixel/Adafruit_NeoPixel.cpp"
 
 static bool WiFi_is_Connected;
 inline void Check_WiFi_Status()
@@ -33,6 +35,8 @@ inline void Check_WiFi_Status()
     }
 }
 
+Adafruit_NeoPixel * LED_strip;
+
 void setup()
 {
     Serial.begin(115200);
@@ -44,6 +48,13 @@ void setup()
     WiFi.begin(MY_WIFI_SSID, MY_WIFI_PASSWORD);
     Check_WiFi_Status();
     //ETS_GPIO_INTR_ENABLE();
+
+    LED_strip = new Adafruit_NeoPixel(169, LED_WIRED_PIN, NEO_GRB + NEO_KHZ800);
+    LED_strip->begin();
+    LED_strip->show();
+    for(int i = 0;i < 169;i++)
+        LED_strip->setPixelColor(i, 255, 0, 255);
+    LED_strip->show();
 }
 
 void loop()
